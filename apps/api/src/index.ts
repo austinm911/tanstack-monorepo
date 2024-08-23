dc4e2976a1a7b0b0b1f399fe9ec37e3b941f8e47
Austin <31991302+austinm911@users.noreply.github.com>
2024-08-22 23:14:30 -0700

    remove logger package

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,8 @@
-import { log } from "@repo/logger";
 import { createServer } from "./server";
 
 const port = process.env.PORT || 5001;
 const server = createServer();
 
 server.listen(port, () => {
-  log(`api running on ${port}`);
+	console.log(`api running on ${port}`);
 });
---
+++ todo.md
@@ -0,0 +1,8 @@
+# Todo
+
+This is a Turborepo starter template with Tanstack Router
+
+- [ ] Add Tanstack Router
+- [ ] Replace Express Server with Hono
+- [ ] Add Drizzle ORM DB Package
+- [ ] Add Core Package for Business Logic
