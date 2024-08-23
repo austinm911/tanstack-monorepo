368a93261fc091bb672e9f97777058f30521100b
Austin <31991302+austinm911@users.noreply.github.com>
2024-08-23 13:55:40 -0700

    add packages template, minor change to react tsconfig

---
+++ packages/_packages-template/tsup.config.ts
@@ -0,0 +1,9 @@
+import { type Options, defineConfig } from "tsup"
+
+export default defineConfig((options: Options) => ({
+	entryPoints: ["src/index.ts"],
+	clean: true,
+	target: "es2022",
+	format: ["esm"],
+	...options,
+}))
