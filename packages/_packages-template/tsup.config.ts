6cefc550c5a73eb75e4d7e9cc28769131ef2ddb6
Austin <31991302+austinm911@users.noreply.github.com>
2024-08-24 10:48:17 -0700

    misc

--- packages/_packages-template/tsup.config.ts
+++ packages/_packages-template/tsup.config.ts
@@ -2,8 +2,8 @@ import { type Options, defineConfig } from "tsup"
 
 export default defineConfig((options: Options) => ({
 	entryPoints: ["src/index.ts"],
-	clean: true,
-	target: "es2022",
+	dts: true,
+	target: "esnext",
 	format: ["esm"],
 	...options,
 }))
