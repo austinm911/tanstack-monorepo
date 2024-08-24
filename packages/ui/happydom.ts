8fe6f95436b6b233b5a7bef4eed10787e7d5330b
Austin <31991302+austinm911@users.noreply.github.com>
2024-08-24 12:53:32 -0700

    fix

---
+++ packages/ui/happydom.ts
@@ -0,0 +1,6 @@
+// https://bun.sh/guides/test/happy-dom
+//! only able to get this to work when the bunfig.toml and this file are in the subpackages, not in the root
+
+import { GlobalRegistrator } from "@happy-dom/global-registrator"
+
+GlobalRegistrator.register()
